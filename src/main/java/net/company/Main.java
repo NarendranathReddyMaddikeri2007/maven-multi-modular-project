2526846cbe317f8e8ed80dfc1dd02f6d16b62987
narendranath <narendranath.maddikeri2007@gmail.com>
2024-01-30 11:31:44 +0530

    deleted Parent src folder

--- src/main/java/net/company/Main.java
+++
@@ -1,8 +0,0 @@
-package net.company;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("It has modules as ");
-    }
-}
